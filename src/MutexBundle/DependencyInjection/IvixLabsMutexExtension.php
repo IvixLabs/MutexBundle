d3b7d8a4562f1057cae98cd96fdcf919deea6b1b
Ivan Ruder <ivixlabs@gmail.com>
2015-06-03 11:05:08 +0600

    Fix bug.

--- src/MutexBundle/DependencyInjection/IvixLabsMutexExtension.php
+++ src/MutexBundle/DependencyInjection/IvixLabsMutexExtension.php
@@ -3,6 +3,7 @@ namespace IvixLabs\MutexBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
